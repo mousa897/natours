189d6e3cbb8ca12bc23f3ad202f64fb8ef6c68fb
Mousa <mousaalihamadna@gmail.com>
2026-03-25 07:40:53 +0300

    error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,22 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.message.match(/"([^"]*)"/)[0];
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -44,6 +60,8 @@ module.exports = (err, req, res, next) => {
     let error = { ...err, message: err.message, name: err.name };
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name == 'ValidationError') error = handleValidationErrorDB(error);
 
     sendErrorProd(error, res);
   }
