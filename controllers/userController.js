5d27cafc65df828c0e63d4b3a4e5e68666a58841
Mousa <mousaalihamadna@gmail.com>
2026-03-28 13:35:15 +0300

    auth

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,27 +1,41 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route has not yet been defined',
+const User = require('./../Models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  // SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
+
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'this route has not yet been defined',
   });
 };
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'this route has not yet been defined',
   });
 };
+
 exports.updateUser = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'this route has not yet been defined',
   });
 };
+
 exports.deleteUser = (req, res) => {
   res.status(500).json({
     status: 'error',
