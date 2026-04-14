4af9b623cb429af9346ba06b53c53bbf548c11ba
Mousa <mousaalihamadna@gmail.com>
2026-04-14 13:48:27 +0300

    password reset

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -43,6 +44,10 @@ const userSchema = new mongoose.Schema({
   },
 
   passwordChangedAt: Date,
+
+  passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
 });
 
 userSchema.pre('save', async function (next) {
@@ -76,6 +81,21 @@ userSchema.methods.passwordChangedAfter = function (JWTTimestamp) {
 
   // False means NOT CHANGED
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  console.log({ resetToken }, this.passwordResetToken);
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
