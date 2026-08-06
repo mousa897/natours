3425bdc3e07e7954c05933643c6f3e9ac737b9ee
Mousa <mousaalihamadna@gmail.com>
2026-08-06 21:43:19 +0300

    fixed bug

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ process.on('uncaughtException', (err) => {
   process.exit(1);
 });
 
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config({ path: './config.env' });
-}
-
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE;
